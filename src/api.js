5c3f860f677f7d117d85faaa2ef6e3fc0f31646d
Joy M <creator@Life-Is-Joy.local>
2021-09-26 13:04:19 +0900

    ADD: api for coins, exchanges,prices

--- src/api.js
+++ src/api.js
@@ -1,1 +1,19 @@
 import axios from "axios";
+
+const api = axios.create({
+    baseURL:" https://api.coinpaprika.com/v1/"
+})
+
+export const priceApi = {
+    tickers:() => api.get("tickers")
+}
+
+
+export const exchangeApi = {
+    exchanges:() => api.get("exchanges")
+}
+
+
+export const coinApi = {
+    coins:() => api.get("coins")
+}
